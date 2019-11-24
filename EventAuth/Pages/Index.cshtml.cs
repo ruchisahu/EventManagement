9cfe48b9578372fe1f74d185a8db793d11ffe4fc
ruchisahu <sahruchi@gmail.com>
2019-11-23 23:44:20 -0800

    added basic authentication

---
+++ EventAuth/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using EventAuth.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace EventAuth.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty, DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Message { get; set; }
+        public async Task<IActionResult> OnPost()
+        {
+            var user = configuration.GetSection("SiteUser").Get<SiteUser>();
+
+            if (UserName == user.UserName)
+            {
+                var passwordHasher = new PasswordHasher<string>();
+                if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, UserName)
+                    };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                    //  return RedirectToPage("/admin/index");
+                    return Redirect("https://localhost:44334/");
+                }
+            }
+            Message = "Invalid attempt";
+            return Page();
+        }
+    }
+}
